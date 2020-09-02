2decb4f741284a0454d23ad0befee9927af00bfd
Tony Pulickal <ttp@hashicorp.com>
2020-09-02 14:21:30 -0400

    remove one AZ

--- main.tf
+++ main.tf
@@ -5,9 +5,9 @@ module "vpc" {
   name = var.name
   cidr = "10.0.0.0/16"
 
-  azs             = ["us-west-1a", "us-west-1b", "us-west-1c"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  azs             = ["us-west-1a", "us-west-1b"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
 
   enable_nat_gateway = true
   enable_vpn_gateway = false
